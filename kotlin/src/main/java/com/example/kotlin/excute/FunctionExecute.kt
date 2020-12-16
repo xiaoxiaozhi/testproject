932d96ee1fa5fadcad108ff0ebe2a315f1b8c73a
zhixun <327354573@qq.com>
2020-12-16 18:27:48 +0800

    lambda表达式

--- kotlin/src/main/java/com/example/kotlin/excute/FunctionExecute.kt
+++ kotlin/src/main/java/com/example/kotlin/excute/FunctionExecute.kt
@@ -1,5 +1,9 @@
 package com.example.kotlin.excute
 
+/**
+ * 官方文档->函数 翻译太差看不懂
+ *
+ */
 fun main(args: Array<String>) {
 
     foo(baz = 1)
@@ -8,16 +12,18 @@ fun main(args: Array<String>) {
     println("中缀表示法${1 double 2}")
     MyStringCollection() add "sadas"//成员函数中使用中缀表示法
 
-
+    println(lambda1(10, 5))
+//    A::foo(1)
 }
 
 //1.默认参数的函数
 fun foo(bar: Int = 0, baz: Int) {
-    println("foo")
+    println("fun foo default arg bar = $bar baz = $baz ")
 }
 
 //2.单表达式函数
 fun double(x: Int): Int = x * 2
+fun double1(x: Int) = x * 2
 
 /**
  * ## 3.中缀表示法
@@ -30,6 +36,7 @@ infix fun Int.double(x: Int) = x * 2
 
 class MyStringCollection {
     infix fun add(s: String) { /*……*/
+        println(s)
     }
 
     fun build() {
@@ -54,6 +61,13 @@ fun foo() = 1        // 良好
 
 //4. 高阶函数：参数类型是函数类型，或者返回值是函数类型的函数称为高阶函数，除此之外函数类型还能赋值给变量
 
+//4.1  lambda 表达式-函数类型来声明函数 :()内是参数->之后是函数返回类新型
+var lambda1: (Int, Int) -> Int = { x, y -> x - y }
+
+//4.1  lambda 表达式-函数类型来声明函数 :带有接收者的函数
+//var lambda2: Math.(Int) -> Unit = { x.(u) -> x-u }
+//4.2 lambda 表达式-挂起函数
+//var lambda2: Math.(Int) -> Unit = { x.(u) -> x-u }
 //可变数量函数 参数需要 vararg 修饰
 fun <T> asList(vararg ts: T): List<T> {
     val result = ArrayList<T>()
--- kotlin/src/main/java/com/example/kotlin/module/KotlinFun.kt
+++
@@ -1,5 +0,0 @@
-package com.myapplication.module
-
-class KotlinFun {
-
-}
